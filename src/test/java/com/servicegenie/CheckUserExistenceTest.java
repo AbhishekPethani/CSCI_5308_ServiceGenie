aaafd08442f0434b1d167f02325dcf39c8a26d96
Kandarp Parikh <kn616924@dal.ca>
2021-11-01 02:10:32 -0300

    Refactor ServiceGenie package to servicegenie

--- src/test/java/com/servicegenie/CheckUserExistenceTest.java
+++ src/test/java/com/servicegenie/CheckUserExistenceTest.java
@@ -1,4 +1,4 @@
-package com.ServiceGenie;
+package com.servicegenie;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/com/servicegenie/ServiceGenieApplicationTests.java
+++ src/test/java/com/servicegenie/ServiceGenieApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ServiceGenie;
+package com.servicegenie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
