2e06c7b3ed67e2db4fae95fefeb33a6786e8e01f
Kandarp Parikh <kn616924@dal.ca>
2021-11-06 16:02:38 -0300

    Revert "Merge branch 'dev_login_module' into 'main'"
    This reverts merge request !4

--- src/main/java/com/ServiceGenie/MyController.java
+++ src/main/java/com/ServiceGenie/MyController.java
@@ -1,28 +1,15 @@
-package com.servicegenie;
+package com.ServiceGenie;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.sql.*;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
+@RestController
 public class MyController {
-	Connection connect;
 
 	@GetMapping("/hello")
 	public String HelloWorld() {
 		return "Hello Group 19, Welcome to MACS program , Good MOrning";
 	}
 
-	
 }
