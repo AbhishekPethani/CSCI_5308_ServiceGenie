db5ba7d0a80d1133e055ac0cf457a4b40a90c994
Abhishek Pareshbhai Pethani <ab823206@dal.ca>
2021-12-09 05:31:28 -0400

    Added Service Provider module

--- src/main/java/com/servicegenie/services/RegisterAsServiceProviderService.java
+++ src/main/java/com/servicegenie/services/RegisterAsServiceProviderService.java
@@ -1,68 +1,37 @@
 package com.servicegenie.services;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import org.springframework.stereotype.Service;
+import com.servicegenie.daos.ServiceProviderProfileDao;
+import com.servicegenie.model.ServiceProviderProfile;
 
-import com.servicegenie.daos.ObtainDatabaseConnectionDao;
+//author
+//Abhishek Pareshbhai Pethani
+//B00899521
 
 //Class to register a new user as a service-provider
 @Service
 public class RegisterAsServiceProviderService {
+	// ServiceProviderProfileDao object
+	private ServiceProviderProfileDao serviceProviderProfileDao; 
 	
+	public RegisterAsServiceProviderService() {
+		this.serviceProviderProfileDao = new ServiceProviderProfileDao();
+	}
 	
-	public String RegisterAsAServiceProvider(String userID, String firstName, String lastName, String email, String contactNumber, String street, String apartmentNumber, String city, String zipcode, String province, String country, String category, String serviceDescription, String branchID, String password) throws SQLException
-	{
-		// Creating instance of CheckUserExistence class to verify if the user of type service-provider is already present or not
-		// If the UserId is present in database, the user is redirected to Registration page with Error message
-		// Else the a new user is registered with service-provider type
-		
-		CheckUserExistenceService validation = new CheckUserExistenceService();
-		
-		if(validation.checkDatabase(userID,"service-provider") == true)
-		{
-			return "redirect:RegistrationFailed.html";
-		}
-		
-		Connection myDBConnect = ObtainDatabaseConnectionDao.getInstance().getMyConnection();
-		
-		String insertServiceProviderQuery = " insert into service_provider_details (ServiceProviderID, ServiceProvider_FirstName,"
-				+ "ServiceProvider_LastName, ServiceProvider_Category, ServiceProvider_CategoryIndex,"
-				+ "ServiceProvider_Email, ServiceProvider_ContactNumber,"
-				+ "ServiceProvider_Apartment, ServiceProvider_Street, ServiceProvider_City,"
-				+ "ServiceProvider_Zipcode, ServiceProvider_Province, ServiceProvider_Country,"
-				+ "ServiceProvider_ServiceDescription, ServiceProvider_BranchID)"
-			    + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		
-		String insertAuthenticationDetailQuery = "insert into user_authentication (User_ID, User_Password, User_Type) values (?, ?, ?)"; 
-		
-		PreparedStatement MyPreparedStatement = myDBConnect.prepareStatement(insertServiceProviderQuery);
-	    MyPreparedStatement.setString (1, userID);
-	    MyPreparedStatement.setString (2, firstName);
-	    MyPreparedStatement.setString (3, lastName);
-	    MyPreparedStatement.setString (4, category);
-	    MyPreparedStatement.setString (5, "12");
-	    MyPreparedStatement.setString (6, email);
-	    MyPreparedStatement.setString (7, contactNumber);
-	    MyPreparedStatement.setString (8, apartmentNumber);
-	    MyPreparedStatement.setString (9, street);
-	    MyPreparedStatement.setString (10, city);
-	    MyPreparedStatement.setString (11, zipcode);
-	    MyPreparedStatement.setString (12, province);
-	    MyPreparedStatement.setString (13, country);
-	    MyPreparedStatement.setString (14, serviceDescription);
-	    MyPreparedStatement.setString (15, branchID);
-	    MyPreparedStatement.execute();
-
-	    MyPreparedStatement = myDBConnect.prepareStatement(insertAuthenticationDetailQuery);
-	    MyPreparedStatement.setString (1, userID);
-	    MyPreparedStatement.setString (2, password);
-	    MyPreparedStatement.setString (3, "service-provider");
-	    MyPreparedStatement.execute();
-	    
-		return "redirect:SuccessfulRegistration.html";
-		
+	public void setServiceProviderProfileDao(ServiceProviderProfileDao serviceProviderProfileDao) {
+		this.serviceProviderProfileDao = serviceProviderProfileDao;
 	}
 
+	public boolean registerAsAServiceProvider(String userID, String password, ServiceProviderProfile profile) throws SQLException{
+		boolean isSuccessfull = serviceProviderProfileDao.addServiceProviderProfile(userID, profile);
+		serviceProviderProfileDao.addServiceProviderAuthenticationDetail(userID, password);
+		return isSuccessfull;	
+	}
+	
+	public boolean checkServiceProviderExistenece(String userID) throws SQLException {
+		// check if service provider is already exist or not
+		boolean isServiceProviderExist = serviceProviderProfileDao.checkIfServiceProviderAlreadyRegistered(userID, "service-provider");
+		return isServiceProviderExist;		
+	}
 }
--- src/main/java/com/servicegenie/services/ServiceProviderAuthenticationService.java
+++ src/main/java/com/servicegenie/services/ServiceProviderAuthenticationService.java
@@ -1,37 +1,34 @@
-//Author
-//Kandarp Sharad Parikh
-//B00873863
-
 package com.servicegenie.services;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
 import org.springframework.stereotype.Service;
+import com.servicegenie.daos.ServiceProviderAuthenticationDao;
 
-import com.servicegenie.ObtainDatabaseConnection;
+//author
+//Abhishek Pareshbhai Pethani
+//B00899521
 
 @Service
 public class ServiceProviderAuthenticationService {
+	// object of AddServiceDao
+	private static ServiceProviderAuthenticationDao serviceProviderAuthenticationDao = new ServiceProviderAuthenticationDao();
 	
-	public String validateUser(String userID , String password) throws SQLException
-	{
-		// Obtain Database connection and get customer details for authentication
-		ObtainDatabaseConnection MyDBConnect = new ObtainDatabaseConnection();
-		Statement sql = MyDBConnect.GetMyConnection().createStatement();
-		ResultSet result = sql.executeQuery("Select * from user_authentication where User_Type='service-provider';");
-				
-		// Check if userid and password entered matches or not
-		// If the userId and password matches the user will be redirected to Customer Home page
-		// Else the User will be redirected to Login page again which has an error message
-		while(result.next())
-		{
-			if(result.getString("User_ID").equals(userID) && result.getString("User_Password").equals(password))
-			{
-				return "ServiceProviderHomePage.html";
-			}
-		}
-		return "LoginFailed.html";
+	public boolean validate(String userID, String password) throws SQLException {
+		// String message for displaying operations result on UI
+		boolean isValidUser = false;
+		isValidUser = serviceProviderAuthenticationDao.validateUser(userID, password);
+		return isValidUser;
+	}	
+	
+	public int countService(String userID) throws SQLException {
+		int count = 0;
+		count = serviceProviderAuthenticationDao.countServicesByServiceProviderID(userID);
+		return count;
+	}
+	
+	public double totalEarning(String userID) throws SQLException {
+		double earning = 0;
+		earning = serviceProviderAuthenticationDao.calculateEarning(userID);
+		return earning;
 	}
 }
